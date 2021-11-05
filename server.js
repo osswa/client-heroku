d38b3a245824b56d31fa7256ec5db41ef18fdf9b
Oswaldo Granados <ogranados@ts4.mx>
2021-11-04 23:18:25 -0600

    guion medio

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/client_heroku'));
+app.use(express.static('./dist/client-heroku'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/client_heroku/'}),
+    res.sendFile('index.html', {root: 'dist/client-heroku/'}),
 );
 
 // Start the app by listening on the default Heroku port
